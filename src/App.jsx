3292fbf4dfbc340fe89622aee837f9a79af364a1
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-09-27 10:19:44 +0600

    added togglr in button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import Available from './components/Available/Available'
 import Banner from './components/Banner'
@@ -12,16 +12,29 @@ const fetchPlayers = async() =>{
 }
 
 function App() {
+  const [toggle,setToggle]=useState(true)
   const playerPromise=fetchPlayers();
   return (
     <>
+    
     <Navbar></Navbar>
     <Banner></Banner>
     {/* 2 Button */}
-    <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+    <div className='flex justify-between items-center mt-10 container mx-auto'>
+      <h1 className='font-bold text-[28px]'>Available Players</h1>
+      <div className='flex'>
+       <button  onClick={() => setToggle(true)}  className={`p-2 border border-gray-200 rounded-l-xl border-r-0 cursor-pointer  ${toggle === true ? "bg-[#E7FE29] font-semibold" : ""}`}>Available</button>
+                                    
+        <button  onClick={() => setToggle(false)}  className={`p-2 border border-gray-200 rounded-r-xl border-l-0 cursor-pointer  ${toggle === false ? "bg-[#E7FE29] font-semibold" : ""}`}>Selected <span>(0)</span></button>
+      </div>
+    </div>
+    {
+      toggle===true ? <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
       <Available playerPromise={playerPromise}></Available>
-    </Suspense>
-    {/* <Selected></Selected> */}
+    </Suspense> : <Selected></Selected>
+    }
+    
+    
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import navImg from "../assets/logo.png";
-import coinImg from "../assets/realistic-lottery-symbol-isolated_23-2151177245.jpg";
+import coinImg from "../assets/Currency.png";
 
 const Navbar = () => {
     return (
@@ -15,7 +15,7 @@ const Navbar = () => {
                     <a href="" className="text-[rgba(19,19,19,0.7)]">Teams</a>
                     <a href="" className="text-[rgba(19,19,19,0.7)]">Schedule</a>
                     <button className="btn btn-ghost flex items-center gap-2 mr-5 shadow rounded-l">
-                        <span className="font-bold">0</span> 
+                        <span className="font-bold">60000000000</span> 
                         <span className="font-bold">Coin</span> 
                         <img src={coinImg} alt="coin" className="w-5 h-5 object-contain"/>
                     </button>
